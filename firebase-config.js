ff257d40ff3f13506f1dcd6591397b53c3a8d26b
yousefiraq <50072463+yousefiraq@users.noreply.github.com>
2025-03-09 22:12:55 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
